f386d1d9383695d6a98767145e4f68ed64c75f48
Nancy Hartman <103781121+nancyah@users.noreply.github.com>
2023-02-24 10:28:46 -0600

    added comments to lines

--- Create a Table.sql
+++ Create a Table.sql
@@ -1,9 +1,13 @@
+-- I created my own table named friends and added/deleted data from it. 
+
+-- I created the table named friends with the columns id, name, and birthday.
 create table friends (
   id integer, 
   name text, 
   birthday date
 );
 
+-- I added 3 friends and their info to the table.
 insert into friends (id, name, birthday)
 values (1, 'Ororo Munroe', '1940-05-30');
 
@@ -13,9 +17,11 @@ values (2, 'Jace', '2014-07-04');
 insert into friends (id, name, birthday)
 values (3, 'Kiko', '2021-08-13');
 
+-- Ororo Munroe changed her name to Storm, because she can control the weather.
 update friends
 set name = 'Storm' where id = 1;
 
+-- I updated everyone's email. First I added an email column, then input the info.
 alter table friends
 add column email text;
 
@@ -31,6 +37,7 @@ update friends
 set email = 'kiko@codecademy.com' 
 where name = 'Kiko';
 
+-- Actually, Storm is fictional...
 delete from friends
 where name is 'Storm';
 

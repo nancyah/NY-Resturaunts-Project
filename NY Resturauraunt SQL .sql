5e61419fd7610db19c7e55ba1c1693d5a96326da
Nancy Hartman <103781121+nancyah@users.noreply.github.com>
2023-02-23 19:40:06 -0600

    Update NY Resturauraunt SQL .sql
    added comments on queries

--- NY Resturauraunt SQL .sql
+++ NY Resturauraunt SQL .sql
@@ -1,13 +1,19 @@
+-- show all columns of table
 select * from nomnom;
 
+-- explore the unique neighborhoods
 select distinct neighborhood from nomnom;
 
+-- explore the unique cuisine choices
 select distinct cuisine from nomnom;
 
+-- I like Chinese food. Do you?
 select name, cuisine from nomnom where cuisine is 'Chinese';
 
+-- Let's go somewhere really yummy!
 select name, review from nomnom where review >= 4;
 
+-- 
 select name, cuisine, price from nomnom where cuisine = 'Italian' and price is '$$$';
 
 select name from nomnom where name like '%meatball%';

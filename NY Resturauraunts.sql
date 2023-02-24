b6c533231575dd41bcf63a8bb7032e6e66a70645
Nancy Hartman <103781121+nancyah@users.noreply.github.com>
2023-02-24 10:10:17 -0600

    Update and rename NY Resturauraunt SQL .sql to NY Resturauraunts.sql

--- NY Resturauraunts.sql
+++ NY Resturauraunts.sql
@@ -1,3 +1,6 @@
+-- There is a table of restaurants called nomnom with columns name, neighborhood, cuisine, review, price, and health. 
+-- I used the SQL queries I learned to find the best dinner spots in the city.
+
 -- getting a feel for the nomnom table, identifying the column names.
 select * from nomnom;
 
